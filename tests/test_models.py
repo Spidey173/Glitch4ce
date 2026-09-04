7532ed485c90c3872272afac57d21c38c6ab202f
Spidey173 <pruthvir6666@gmail.com>
2026-09-05 01:51:56 +0530

    test(models): add unit tests for GameScore model and player total score properties

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,6 @@
 """Unit tests for SQLAlchemy models."""
 from app.extensions import db
-from app.models import GameplaySession, User
+from app.models import GameScore, GameplaySession, User
 
 
 def test_user_creation_and_password_hashing(app):
@@ -38,17 +38,39 @@ def test_gameplay_session_relationship(app, test_user):
         assert 'Flappy Bird' in game_names
 
 
+def test_game_score_model_and_highest_score(app, test_user):
+    """Test GameScore model, user highest_score property, and dictionary serialization."""
+    with app.app_context():
+        user = db.session.get(User, test_user.id)
+        assert user.highest_score == 0
+
+        score1 = GameScore(player_id=user.id, game_name='2048', score=1200)
+        score2 = GameScore(player_id=user.id, game_name='Flappy Bird', score=4500)
+        db.session.add_all([score1, score2])
+        db.session.commit()
+
+        assert user.highest_score == 4500
+        score_dict = score2.to_dict()
+        assert score_dict['score'] == 4500
+        assert score_dict['game_name'] == 'Flappy Bird'
+        assert score_dict['username'] == user.username
+
+
 def test_cascade_delete(app, test_user):
-    """Test that deleting a user cascades to their gameplay sessions."""
+    """Test that deleting a user cascades to their gameplay sessions and scores."""
     with app.app_context():
         user = db.session.get(User, test_user.id)
         session1 = GameplaySession(player_id=user.id, game_name='Maze')
-        db.session.add(session1)
+        score1 = GameScore(player_id=user.id, game_name='Maze', score=900)
+        db.session.add_all([session1, score1])
         db.session.commit()
 
         assert GameplaySession.query.filter_by(player_id=user.id).count() == 1
+        assert GameScore.query.filter_by(player_id=user.id).count() == 1
 
         db.session.delete(user)
         db.session.commit()
 
         assert GameplaySession.query.filter_by(player_id=test_user.id).count() == 0
+        assert GameScore.query.filter_by(player_id=test_user.id).count() == 0
+
